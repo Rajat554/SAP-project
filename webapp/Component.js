4b718d9004a6becbcb1f649bc1d22da8308100d9
Rajat patel <rajatpatel08e@gmail.com>
2026-04-05 20:29:52 +0530

    this is my first commit

---
+++ webapp/Component.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel"
+], function (UIComponent, JSONModel) {
+	"use strict";
+
+	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * @public
+		 * @override
+		 */
+		init: function () {
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// create the views based on the url/hash
+			this.getRouter().initialize();
+		}
+	});
+});
